96bd26049f6de7ccc7b91599f639b767bce10739
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2022-12-06 20:54:41 +0100

    Day 6

---
+++ src/day06/index.js
@@ -0,0 +1,29 @@
+import run from "aocrunner";
+
+const findMarker = (input, numDistinctCharacters) => {
+  for (let i = numDistinctCharacters; i <= input.length; i++) {
+    let packet = input.slice(i - numDistinctCharacters, i);
+    let received = new Set(packet);
+    if (received.size == numDistinctCharacters) {
+      return i;
+    }
+  }
+  return undefined;
+}
+
+const part1 = (input) => findMarker(input, 4);
+
+const part2 = (input) => findMarker(input, 14);
+
+run({
+  part1: {
+    tests: [],
+    solution: part1,
+  },
+  part2: {
+    tests: [],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
