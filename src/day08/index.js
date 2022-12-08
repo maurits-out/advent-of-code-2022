693bcdcc1fb729f4f7288f478428526871863fef
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2022-12-08 21:18:58 +0100

    Added test

--- src/day08/index.js
+++ src/day08/index.js
@@ -46,11 +46,33 @@ const part2 = (rawInput) => {
 
 run({
   part1: {
-    tests: [],
+    tests: [
+      {
+        input: `
+        30373
+        25512
+        65332
+        33549
+        35390
+      `,
+        expected: 21,
+      },
+    ],
     solution: part1,
   },
   part2: {
-    tests: [],
+    tests: [
+      {
+        input: `
+        30373
+        25512
+        65332
+        33549
+        35390
+      `,
+        expected: 8,
+      },
+    ],
     solution: part2,
   },
   trimTestInputs: true,
