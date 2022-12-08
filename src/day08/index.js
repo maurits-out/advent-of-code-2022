3772db8360d1afaf8184324880a73c45cb9e16e4
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2022-12-08 19:33:49 +0100

    Small improvements.

--- src/day08/index.js
+++ src/day08/index.js
@@ -11,36 +11,36 @@ const parseInput = (rawInput) =>
   rawInput.split("\n").map((line) => line.split("").map((ch) => +ch));
 
 const isVisible = (heightMap, row, column) => {
-  let left = Math.max(...heightMap[row].slice(0, column));
-  let right = Math.max(...heightMap[row].slice(column + 1));
-  let up = Math.max(...heightMap.slice(0, row).map((r) => r[column]));
-  let down = Math.max(...heightMap.slice(row + 1).map((r) => r[column]));
-  let height = heightMap[row][column];
+  const left = Math.max(...heightMap[row].slice(0, column));
+  const right = Math.max(...heightMap[row].slice(column + 1));
+  const up = Math.max(...heightMap.slice(0, row).map((r) => r[column]));
+  const down = Math.max(...heightMap.slice(row + 1).map((r) => r[column]));
+  const height = heightMap[row][column];
   return height > left || height > right || height > up || height > down;
 };
 
 const distance = (height, trees) => {
-  let d = range(trees.length).filter((t) => trees[t] >= height);
+  const d = range(trees.length).filter((t) => trees[t] >= height);
   return d.length > 0 ? d[0] + 1 : trees.length;
 }
 
 const scenicScore = (heightMap, row, column) => {
-  let left = heightMap[row].slice(0, column).reverse();
-  let right = heightMap[row].slice(column + 1);
-  let up = heightMap.slice(0, row).map((r) => r[column]).reverse();
-  let down = heightMap.slice(row + 1).map((r) => r[column]);
-  let height = heightMap[row][column];
+  const left = heightMap[row].slice(0, column).reverse();
+  const right = heightMap[row].slice(column + 1);
+  const up = heightMap.slice(0, row).map((r) => r[column]).reverse();
+  const down = heightMap.slice(row + 1).map((r) => r[column]);
+  const height = heightMap[row][column];
   return distance(height, left) * distance(height, right) * distance(height, up) * distance(height, down)
 };
 
 const part1 = (rawInput) => {
-  let heightMap = parseInput(rawInput);
+  const heightMap = parseInput(rawInput);
   return coordinates(heightMap).filter(([r, c]) => isVisible(heightMap, r, c)).length;
 };
 
 const part2 = (rawInput) => {
-  let heightMap = parseInput(rawInput);
-  let scores = coordinates(heightMap).map(([r, c]) =>
+  const heightMap = parseInput(rawInput);
+  const scores = coordinates(heightMap).map(([r, c]) =>
     scenicScore(heightMap, r, c),
   );
   return Math.max(...scores);
