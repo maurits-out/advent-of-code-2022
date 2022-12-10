e3d17c0548d368b105ca15b9a25e3f616a1f2ffb
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2022-12-10 16:12:10 +0100

    Improvements

--- src/day01/index.js
+++ src/day01/index.js
@@ -1,31 +1,76 @@
 import run from "aocrunner";
 
-const calculateSum = (array) => array.reduce((acc, n) => acc + n, 0);
+function sum(numbers) {
+  return numbers.reduce((acc, n) => acc + n, 0);
+}
 
-const parseSection = (section) => section.split("\n").map((num) => +num);
+function parseSection(section) {
+  return section.split("\n").map((num) => Number(num));
+}
 
-const parseInput = (rawInput) =>
-  rawInput.split("\n\n").map(parseSection).map(calculateSum);
+function parseInput(rawInput) {
+  return rawInput.split("\n\n").map(parseSection).map(sum);
+}
 
-const part1 = (rawInput) => {
+function part1(rawInput) {
   const calorieSummations = parseInput(rawInput);
   return Math.max(...calorieSummations);
-};
+}
 
-const part2 = (rawInput) => {
+function part2(rawInput) {
   const calorieSummations = parseInput(rawInput);
-  return calculateSum(calorieSummations.sort().slice(-3));
-};
+  return sum(calorieSummations.sort((a, b) => a - b).slice(-3));
+}
 
 run({
   part1: {
-    tests: [],
-    solution: part1,
+    tests: [
+      {
+        input: `
+        1000
+        2000
+        3000
+        
+        4000
+        
+        5000
+        6000
+        
+        7000
+        8000
+        9000
+        
+        10000
+      `,
+        expected: 24000
+      }
+    ],
+    solution: part1
   },
   part2: {
-    tests: [],
-    solution: part2,
+    tests: [
+      {
+        input: `
+        1000
+        2000
+        3000
+        
+        4000
+        
+        5000
+        6000
+        
+        7000
+        8000
+        9000
+        
+        10000
+      `,
+        expected: 45000
+      }      
+    ],
+    solution: part2
   },
   trimTestInputs: true,
-  onlyTests: false,
+  onlyTests: false
 });
